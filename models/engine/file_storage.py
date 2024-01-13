cea46c3c3afa2044b13bd6265d116476bd6921de
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-01-13 16:19:28 +0200

    whole storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,43 +1,55 @@
 #!/usr/bin/python3
-"""_
+"""
 Module: file_storage.py
 
-has a FileStorage class
+Defines a `FileStorage` class.
 """
+import os
 import json
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
+from models.review import Review
 from models.amenity import Amenity
 from models.place import Place
-from models.review import Review
-import os
 
 
-class FileStorage:
-    """Serializes and deserializes JSOM file to instances"""
+class FileStorage():
+    """
+    serializes instances to a JSON file and
+    deserializes JSON file to instances
+    """
 
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Returns dictionary __objects."""
+        """
+        returns the dictionary __objects
+        """
         return FileStorage.__objects
 
     def new(self, obj):
-        """Sets __objects the obj with key <obj class name>.id."""
+        """
+        sets in __objects the obj with key <obj class name>.id
+        """
         key = "{}.{}".format(type(obj).__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """Serializes the __objects to the JSON file (path: __file_path)."""
+        """
+        serializes __objects to the JSON file (path: __file_path)
+        """
         with open(FileStorage.__file_path, 'w') as f:
             json.dump(
                 {k: v.to_dict() for k, v in FileStorage.__objects.items()}, f)
 
     def reload(self):
-        """Deserializes the JSON file to __objects."""
+        """
+        deserializes the JSON file to __objects only if the JSON
+        file exists; otherwise, does nothing
+        """
         current_classes = {'BaseModel': BaseModel, 'User': User,
                            'Amenity': Amenity, 'City': City, 'State': State,
                            'Place': Place, 'Review': Review}
