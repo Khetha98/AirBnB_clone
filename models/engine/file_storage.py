cac1ce585bf84e3b089d0ec9a999aaebe8f81262
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-01-13 15:11:32 +0200

    comments

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""_
+Module: file_storage.py
 
+has a FileStorage class
+"""
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -10,7 +14,10 @@ from models.place import Place
 from models.review import Review
 import os
 
+
 class FileStorage:
+    """Serializes and deserializes JSOM file to instances"""
+
     __file_path = "file.json"
     __objects = {}
 
