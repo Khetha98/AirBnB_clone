60cf45cf98878f316e595bcb115e5a59c112b54b
Khsimam022 <Khsimam022@student.wethinkcode.co.za>
2024-01-13 20:37:18 +0200

    tests

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-"""This a module  base.py"""
-from datetime import datetime
-import uuid
+"""
+Module: base.py
+"""
 import models
+import uuid
+from datetime import datetime
 
 
-class BaseModel:
+class BaseModel():
     """
-    It defines attributes or methods that are common
-    to other classes
+    Base class which defines all common
+    attributes/methods for other classes
     """
 
     def __init__(self, *args, **kwargs):
-        """Instantiates an object with its attributes."""
-        if len(kwargs) != 0:
+        """
+        instatiates an object with it's
+        attributes
+        """
+        if len(kwargs) > 0:
             for key, value in kwargs.items():
                 if key == '__class__':
                     continue
@@ -22,31 +27,36 @@ class BaseModel:
                 setattr(self, key, value)
             return
 
-
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
         models.storage.new(self)
 
     def __str__(self):
         """
-        Gives out a string representation
+        Returns the string representation
         of the instance
         """
         return "[{}] ({}) {}".format(
             type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """It updates public instance attributes"""
-        self.updated_at = datetime.utcnow()
+        """
+        updates the public instance attribute
+        updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """Gives a dictionary derived from __dict__"""
-        map_obj = self.__dict__.copy()
-        map_obj['created_at'] = self.created_at.isoformat()
-        map_obj['updated_at'] = self.updated_at.isoformat()
-        map_obj['__class__'] = self.__class__.__name__
+        """
+        returns a dictionary containing all keys/values
+        of __dict__ of the instance
+        """
+        dict = {**self.__dict__}
+        dict['__class__'] = type(self).__name__
+        dict['created_at'] = dict['created_at'].isoformat()
+        dict['updated_at'] = dict['updated_at'].isoformat()
 
-        return map_obj
+        return dict
